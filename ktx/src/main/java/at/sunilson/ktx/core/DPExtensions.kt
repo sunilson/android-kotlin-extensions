0451541fdc2a81525d9da76c17099306ba02c0d9
Linus Weiss <weisslinus@gmail.com>
2020-04-03 09:37:59 +0200

    Add SystemUiExtensions

--- ktx/src/main/java/at/sunilson/ktx/core/DPExtensions.kt
+++ ktx/src/main/java/at/sunilson/ktx/core/DPExtensions.kt
@@ -6,7 +6,7 @@ import android.util.DisplayMetrics
 fun Int.convertToPx(context: Context) =
     (this * (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).toInt()
 
-fun Int.convertToDp(contefxt: Context) =
+fun Int.convertToDp(context: Context) =
     (this / (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).toInt()
 
 fun Float.convertToPx(context: Context) =
