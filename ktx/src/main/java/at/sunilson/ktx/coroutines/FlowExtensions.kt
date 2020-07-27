3b2b848bfad99207bf6849da3d86a5d84b21f419
Linus Weiss <linus@arkulpa.at>
2020-07-27 11:36:21 +0200

    Add combineAsync extension

---
+++ ktx/src/main/java/at/sunilson/ktx/coroutines/FlowExtensions.kt
@@ -0,0 +1,32 @@
+package at.sunilson.ktx.coroutines
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+/**
+ * Emits results of multiple flow in the order they arrive. Doesn't wait for all flows to emit something
+ * before it starts emitting.
+ *
+ * For 4 flows (A, B, C, D), emissions could look like this:
+ * 
+ * []
+ * [A, C]
+ * [A, C, D]
+ * [A, B, C, D]
+ */
+@ExperimentalCoroutinesApi
+inline fun <reified T : Any> List<Flow<T>>.combineAsync() = channelFlow {
+    val results = Array<T?>(size) { null }
+    send(results.filterNotNull())
+    forEachIndexed { index, flow ->
+        launch {
+            flow.collect {
+                results[index] = it
+                send(results.filterNotNull())
+            }
+        }
+    }
+}
