851ced91d07a67d8df5b57fe47d3832894b1ecf4
Linus Weiss <linus@arkulpa.at>
2020-08-02 16:30:33 +0200

    Add requires API notation

--- ktx/src/main/java/at/sunilson/ktx/datetime/InstantExtensions.kt
+++ ktx/src/main/java/at/sunilson/ktx/datetime/InstantExtensions.kt
@@ -1,7 +1,10 @@
 package at.sunilson.ktx.datetime
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
+@RequiresApi(Build.VERSION_CODES.O)
 fun Long.toZonedDateTime(): ZonedDateTime = Instant.ofEpochMilli(this).atZone(ZoneId.systemDefault())
