176335803bafe5780d3fa3d6b385425ef15ea2f0
Linus Weiss <linus@arkulpa.at>
2020-11-09 15:50:38 +0100

    Update keyboard and system ui stuff to Android R Compat

--- ktx/src/main/java/at/sunilson/ktx/fragment/SystemUiExtensions.kt
+++ ktx/src/main/java/at/sunilson/ktx/fragment/SystemUiExtensions.kt
@@ -9,6 +9,7 @@ import android.view.WindowManager
 import androidx.annotation.ColorRes
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 
 fun Fragment.setStatusBarColor(@ColorRes color: Int, receiver: Window? = activity?.window) {
@@ -25,6 +26,13 @@ fun Fragment.setNavigationBarColor(@ColorRes color: Int, receiver: Window? = act
         addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         navigationBarColor = ContextCompat.getColor(requireContext(), color)
     }
+}
+
+/**
+ * Draw below status and navigation bar
+ */
+fun Fragment.edgeToEdge(enabled: Boolean, receiver: Window? = requireActivity().window) {
+    WindowCompat.setDecorFitsSystemWindows(receiver ?: return, !enabled)
 }
 
 fun Fragment.drawBelowStatusBar(
--- ktx/src/main/java/at/sunilson/ktx/view/KeyboardExtensions.kt
+++ ktx/src/main/java/at/sunilson/ktx/view/KeyboardExtensions.kt
@@ -1,16 +1,23 @@
 package at.sunilson.ktx.view
 
 import android.content.Context
+import android.os.Build
 import android.view.View
 import android.view.ViewTreeObserver
 import android.view.inputmethod.InputMethodManager
 import android.view.inputmethod.InputMethodManager.HIDE_NOT_ALWAYS
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
 /**
  * See this blog post for more information [https://developer.squareup.com/blog/showing-the-android-keyboard-reliably]
  */
 fun View.showKeyboard() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        windowInsetsController?.show(WindowInsetsCompat.Type.ime())
+        return
+    }
+
     fun View.showTheKeyboardNow() {
         if (isFocused) {
             post {
@@ -38,6 +45,11 @@ fun View.showKeyboard() {
 }
 
 fun View.hideKeyboard() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        windowInsetsController?.hide(WindowInsetsCompat.Type.ime())
+        return
+    }
+
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
     imm?.hideSoftInputFromWindow(windowToken, HIDE_NOT_ALWAYS)
 }
