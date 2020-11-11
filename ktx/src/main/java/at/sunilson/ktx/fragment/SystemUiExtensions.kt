090a885fab10882146fc283ef48743dd7857d16a
Linus Weiss <linus@arkulpa.at>
2020-11-11 21:40:53 +0100

    Add status and navigation bar color from theme

--- ktx/src/main/java/at/sunilson/ktx/fragment/SystemUiExtensions.kt
+++ ktx/src/main/java/at/sunilson/ktx/fragment/SystemUiExtensions.kt
@@ -5,10 +5,12 @@ package at.sunilson.ktx.fragment
 import android.os.Build
 import android.view.Window
 import android.view.WindowManager
+import androidx.annotation.AttrRes
 import androidx.annotation.ColorRes
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import at.sunilson.ktx.context.getThemeColor
 import at.sunilson.ktx.window.*
 
 fun Fragment.setStatusBarColor(@ColorRes color: Int, receiver: Window? = activity?.window) {
@@ -19,11 +21,27 @@ fun Fragment.setStatusBarColor(@ColorRes color: Int, receiver: Window? = activit
     }
 }
 
+fun Fragment.setStatusBarThemeColor(@AttrRes color: Int, receiver: Window? = activity?.window) {
+    receiver?.run {
+        clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        statusBarColor = requireContext().getThemeColor(color)
+    }
+}
+
 fun Fragment.setNavigationBarColor(@ColorRes color: Int, receiver: Window? = activity?.window) {
     receiver?.run {
         clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         navigationBarColor = ContextCompat.getColor(requireContext(), color)
+    }
+}
+
+fun Fragment.setNavigationBarThemeColor(@AttrRes color: Int, receiver: Window? = activity?.window) {
+    receiver?.run {
+        clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        navigationBarColor = requireContext().getThemeColor(color)
     }
 }
 
